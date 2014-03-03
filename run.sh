1853ffe58940d4121027bf7fd1a648ff8cc7b17c
Ashlin Eldridge <ashlin.eldridge@gmail.com>
2014-03-03 01:53:38 +0000

    Added ability to specify local port for Go server.

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cd $(dirname $0)
 LOCAL_IP=$(wget -qO- http://instance-data/latest/meta-data/local-ipv4)
-docker run -i -t -p $LOCAL_IP:80:8153 ashlineldridge/go-server $@
+PORT=${1:-8153}
+docker run -i -t -p $LOCAL_IP:$PORT:8153 ashlineldridge/go-server $@
