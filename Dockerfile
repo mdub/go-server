eeb810739083683728b79be4138e2d8ed4c97461
Ashlin Eldridge <ashlin.eldridge@gmail.com>
2014-03-03 05:26:10 +0000

    Added exposure of HTTPS port.

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,9 @@ RUN apt-get install -y unzip
 RUN wget http://download01.thoughtworks.com/go/13.4.1/ga/go-server-13.4.1-18342.deb -O /tmp/go-server.deb
 RUN dpkg -i /tmp/go-server.deb
 
-# Go runs on port 8153 by default
+# Expose ports for HTTP and HTTPS
 EXPOSE 8153
+EXPOSE 8154
 
 ADD start.sh /usr/local/bin/start
 
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 GO_SERVER=$(wget -qO- http://instance-data/latest/meta-data/local-ipv4)
-docker run -i -t -p $GO_SERVER:80:8153 -p $GO_SERVER:8153:8153 -p $GO_SERVER:8154:8154 ashlineldridge/go-server $@
+docker run -i -t -p $GO_SERVER:80:8153 -p $GO_SERVER:443:8154 -p $GO_SERVER:8153:8153 -p $GO_SERVER:8154:8154 ashlineldridge/go-server $@
