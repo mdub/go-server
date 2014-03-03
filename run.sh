63ac97d3937afef762420631444c667eed2fcce3
Ashlin Eldridge <ashlin.eldridge@gmail.com>
2014-03-03 05:20:08 +0000

    Refactored build and run scripts.

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-cd $(dirname $0)
-LOCAL_IP=$(wget -qO- http://instance-data/latest/meta-data/local-ipv4)
-PORT=${1:-8153}
-docker run -i -t -p $LOCAL_IP:$PORT:8153 ashlineldridge/go-server $@
+
+GO_SERVER=$(wget -qO- http://instance-data/latest/meta-data/local-ipv4)
+docker run -i -t -p $GO_SERVER:80:8153 -p $GO_SERVER:8153:8153 -p $GO_SERVER:8154:8154 ashlineldridge/go-server $@
